03fc0f616193d0e38686f7fa1a1840b825ee949e
Alagbala Damilola <zionlloyd05@gmail.com>
2020-07-08 19:47:25 +0100

    Partially completed Banking Microservice

---
+++ MicroRabbit.Banking.Data/IDesignTimeDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using MicroRabbit.Banking.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MicroRabbit.Banking.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<BankingDbContext>
+    {
+
+        BankingDbContext IDesignTimeDbContextFactory<BankingDbContext>.CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+
+            var builder = new DbContextOptionsBuilder<BankingDbContext>();
+            var connectionString = configuration.GetConnectionString("BankingDbConnection");
+
+            //builder.UseSqlServer(connectionString);
+            builder.UseSqlServer(connectionString);
+
+            return new BankingDbContext(builder.Options);
+        }
+    }
+}
---
+++ MicroRabbit.Banking.Data/Repository/AccountRepository.cs
@@ -0,0 +1,24 @@
+﻿using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Banking.Data.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly BankingDbContext ctx;
+
+        public AccountRepository(BankingDbContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public IEnumerable<Account> GetAccounts()
+        {
+            return this.ctx.Accounts;
+        }
+    }
+}
